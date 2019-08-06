1ae82260e253794ac30a5413910f9ade696a5fb2
Joey Van Lierop <joey.vanlierop@me.com>
2019-08-06 17:49:02 -0600

    Improved front-end visuals. Working on scripting next

---
+++ resources/js/libscript.ts
@@ -0,0 +1,24 @@
+//TODO:
+//Clicking table selects checkbox
+//Searchbar
+//Template for table?
+
+let library: Book[] = [];
+
+class Book {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+function addToLibrary(book: Book) {
+
+}
+
+function render(books: Book[]) {
+    books.forEach(book => {
+        
+    });
+}
